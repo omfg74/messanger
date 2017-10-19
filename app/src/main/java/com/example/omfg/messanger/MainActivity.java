0e3ca3037362eb74b7f411c733892b365b5d35b1
Alexander <podsh74@gmail.com>
2017-10-19 11:35:09 +0500

    1123

--- app/src/main/java/com/example/omfg/messanger/MainActivity.java
+++ app/src/main/java/com/example/omfg/messanger/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.omfg.messanger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -10,6 +13,12 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView status, message, income;
+    Button sendBtn;
+    DataInputStream in;
+    DataOutputStream out;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +28,26 @@ public class MainActivity extends AppCompatActivity {
         Socket socket = connection.connect();
         DataFlow dataFlow = new DataFlow();
         try {
-            DataInputStream in = dataFlow.dataControlIn(socket);
-            DataOutputStream outputStream = dataFlow.dataControlOut(socket);
+           in = dataFlow.dataControlIn(socket);
+            out = dataFlow.dataControlOut(socket);
         } catch (IOException e) {
             Log.d("Data log",""+e);
         }
+
+        status = (TextView)findViewById(R.id.send_text);
+        message = (TextView)findViewById(R.id.send_text);
+        income = (TextView)findViewById(R.id.income_text);
+        sendBtn = (Button)findViewById(R.id.send_btn);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = sendBtn.getText().toString();
+                sendMessage(text,out);
+
+            }
+        });
+
 
     }
 
